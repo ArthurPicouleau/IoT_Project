f26782bb1c8cade99a8c17c28bcf83ad4e7c4337
ArthurPicouleau <144266199+ArthurPicouleau@users.noreply.github.com>
2023-12-05 13:47:08 +0200

    Update gcoap_cli.c

--- COAP_Communication/gcoap_cli.c
+++ COAP_Communication/gcoap_cli.c
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2015-2017 Ken Bannister. All rights reserved.
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
-/**
- * @ingroup     examples
- * @{
- *
- * @file
- * @brief       gcoap CLI support
- *
- * @author      Ken Bannister <kb2ma@runbox.com>
- *
- * @}
- */
 
 #include <stdint.h>
 #include <stdio.h>
